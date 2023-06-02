5dd3d73dacd5df881dab62e4c94fe5fd8b393feb
laura.menezes <laura.menezes@crazytechlabs.com.br>
2023-06-02 14:04:10 -0300

    Arquivo main do projeto

---
+++ led-project/main-led.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+//#include "led_strip.h"
+#include "sdkconfig.h"
+
+
+#define LED_PIN  27
+#define PUSH_BUTTON_PIN  33
+
+
+
+void app_main(void)
+{
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);   
+    gpio_set_direction(PUSH_BUTTON_PIN, GPIO_MODE_INPUT);
+
+    gpio_config_t button_conf = {
+        .pin_bit_mask = (1ULL << PUSH_BUTTON_PIN),
+        .mode = GPIO_MODE_INPUT,
+        //.pull_up_en = GPIO_PULLUP_ENABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+
+    gpio_config(&button_conf);
+
+
+    while(1) {       
+        if (gpio_get_level(PUSH_BUTTON_PIN) == 1)
+        {  
+            gpio_set_level(LED_PIN, 1);        
+        } 
+        else
+        {
+            gpio_set_level(LED_PIN, 0);        
+        }
+
+        vTaskDelay(1);
+    }
+} 
